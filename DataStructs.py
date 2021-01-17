d1fe599f0cc8a85b2ea147f47b774a5f332f8eae
Serhii Marinchenko <sa.marinchenko@gmail.com>
2021-01-17 19:36:50 +0200

    add stacks

--- DataStructs.py
+++ DataStructs.py
@@ -12,7 +12,7 @@ def curry(f):
     return curried
 
 
-class dops: #Class contain data operations
+class Dops: #Class contain data operations
     def getNodesVals(self, node, acc):
         acc.append(str(node.get_value()))
         next_node = node.get_next_node()
@@ -142,7 +142,7 @@ class dops: #Class contain data operations
 #print("element {0} is located at index {1}".format(288, result))
 
 #Testing sparse_search
-dops.sparse_search(["Alex", "", "", "", "", "Devan", "", "", "Elise", "", "", "", "Gary", "", "", "Mimi", "", "", "Parth", "", "", "", "Zachary"], "Parh")
+#Dops.sparse_search(["Alex", "", "", "", "", "Devan", "", "", "Elise", "", "", "", "Gary", "", "", "Mimi", "", "", "Parth", "", "", "", "Zachary"], "Parh")
 
 
 
@@ -162,7 +162,7 @@ class Node:
         self.next_node = next_node
 
 
-class LinkedList(dops):
+class LinkedList(Dops):
     def __init__(self, value=None):
         self.head_node = Node(value)
   
@@ -310,5 +310,132 @@ class DoublyLinkedList:
 #print(aux.binary_search_iterative([5,6,7,8,9], 9))
 #print(aux.binary_search_iterative([5,6,7,8,9], 10))
 #print(aux.binary_search_iterative([5,6,7,8,9], 8))
-#print(aux.binary_search_iterative([5,6,7,8,9], 4))
-#print(aux.binary_search_iterative([5,6,7,8,9], 6))
+
+
+#---------------------
+#Queues
+#---------------------
+
+
+class Queue:
+    def __init__(self, max_size=None):
+        self.head = None
+        self.tail = None
+        self.max_size = max_size
+        self.size = 0
+    
+    def enqueue(self, value):
+        if self.has_space():
+            item_to_add = Node(value)
+            print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+            if self.is_empty():
+                self.head, self.tail = item_to_add, item_to_add
+            else:
+                self.tail.set_next_node(item_to_add)
+                self.tail = item_to_add
+            self.size += 1
+        else:
+            print("Sorry, no more room!")
+    
+    def dequeue(self):
+        if not self.is_empty():
+            item_to_remove = self.head
+            print("Removing " + str(item_to_remove.get_value()) + " from the queue!")
+            if self.size == 1:
+                self.head, self.tail = None, None
+            else:
+                self.head = self.head.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print("This queue is totally empty!")
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.head.get_value()
+        else:
+            print ("Nothing to see here!")
+            
+    def get_size(self):
+        return self.size
+    
+    def has_space(self):
+        return (not self.max_size) or (self.max_size > self.get_size())
+    
+    def is_empty(self):
+        return self.size == 0
+
+
+#Uncomment to test
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue(10)
+print("Adding orders to our deli line...\n------------")
+#deli_line.enqueue("egg and cheese on a roll")
+#deli_line.enqueue("bacon, egg, and cheese on a roll")
+#deli_line.enqueue("toasted sesame bagel with butter and jelly")
+#deli_line.enqueue("toasted roll with butter")
+#deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
+#deli_line.enqueue("two fried eggs with home fries and ketchup")
+#deli_line.enqueue("egg and cheese on a roll with jalapeos")
+#deli_line.enqueue("plain bagel with plain cream cheese")
+#deli_line.enqueue("blueberry muffin toasted with butter")
+#deli_line.enqueue("bacon, egg, and cheese on a roll")
+
+#deli_line.enqueue("western omelet with home fries")
+
+#print("------------\nOur first order will be " + deli_line.peek())
+#print("------------\nNow serving...\n------------")
+#for i in range(11):
+#    deli_line.dequeue()
+
+
+#---------------------
+#Stacks
+#---------------------
+
+
+class Stack:
+    def __init__(self, limit=1000):
+        self.top_item = None
+        self.size = 0
+        self.limit = limit
+    
+    def push(self, value):
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size +=1
+            print("Adding {} to the pizza stack!".format(value))
+        else:
+            print("No room for {}!".format(value))
+    
+    def pop(self):
+        if not self.is_empty():
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            print("Delivering " + item_to_remove.get_value())
+            return item_to_remove.get_value()
+        print("All out of pizza.")
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.top_item.get_value()
+        
+    def has_space(self):
+        return self.limit > self.size
+    
+    def is_empty(self):
+        return self.size == 0
+
+#Testing Stacks
+#pizza_stack = Stack(6)
+#for i in range(6):
+#    pizza_stack.push("pizza {0}".format('#' + str(i)))
+
+#pizza_stack.push("pizza #7")
+
+#print("The first pizza to deliver is " + pizza_stack.peek())
+#for i in range(7):
+#    pizza_stack.pop()
